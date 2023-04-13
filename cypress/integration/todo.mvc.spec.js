60b1d3acae180fd08ab1b0dbc109a06639ad8c9f
josdem <joseluis.delacruz@gmail.com>
2023-04-13 19:51:12 -0400

    Adding a todo action

--- cypress/integration/todo.mvc.spec.js
+++ cypress/integration/todo.mvc.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
-it('should navigate to the todomvc', () => {
+it('it should be able to add a new todo to the list', () => {
     cy.visit('http://todomvc-app-for-testing.surge.sh/')
+    cy.get('.new-todo').type("Clean room{enter}")
+    cy.get('.toggle').click()
 })
